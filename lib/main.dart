f2d31067a36ecdeddbf11b69bedf42d82a07e8ee
Rahul Tilwani <tilwani03@gmail.com>
2020-07-23 20:18:36 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text(
+            'I am Rich',
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Center(
+            child: Image(image: AssetImage('images/back.jpg')),
+          ),
+        ),
+      ),
+    ),
+  );
+}
